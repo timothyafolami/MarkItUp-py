75af2335a239a47ac86531550483a3fde96a4e9b
Timmy <timmy@upstridelabs.com>
2026-07-08 22:32:33 +0100

    ner version update

--- markitup/__init__.py
+++ markitup/__init__.py
@@ -19,7 +19,7 @@ from .render_pdf import render_pdf
 from .stamp import stamp
 from .fonts import list_fonts, available_fonts, is_available, SAFE_FONTS
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __all__ = [
     "MarkItUp", "Theme", "Watermark", "Banner", "Running", "Table",
     "parse", "render_docx", "render_html", "render_pdf",
